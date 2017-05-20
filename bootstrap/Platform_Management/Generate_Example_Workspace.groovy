3f6016eb7a38e64b0fedbb9a5da08fcda4e8003f
nikolaos.karapanos <nikolaos.karapanos@accenture.com>
2017-05-20 18:23:00 +0100

    fixing a code issue

--- bootstrap/Platform_Management/Generate_Example_Workspace.groovy
+++ bootstrap/Platform_Management/Generate_Example_Workspace.groovy
@@ -24,13 +24,12 @@ generateExampleWorkspaceJob.with{
     }
     definition {
         cps {
-            script('''// Setup Workspace
-build job: 'Workspace_Management/Generate_Workspace', parameters: [[$class: 'StringParameterValue', name: 'WORKSPACE_NAME', value: "${workspaceName}"]
-
+          script('''// Setup Workspace
+build job: 'Workspace_Management/Generate_Workspace', parameters: [[$class: 'StringParameterValue', name: 'WORKSPACE_NAME', value: "${workspaceName}"]]
 // Setup Faculty
-build job: "${workspaceName}/Project_Management/Generate_Project", parameters: [[$class: 'StringParameterValue', name: 'PROJECT_NAME', value: "${projectName}"]
+build job: "${workspaceName}/Project_Management/Generate_Project", parameters: [[$class: 'StringParameterValue', name: 'PROJECT_NAME', value: "${projectName}"]]
 retry(5) {
-    build job: "${workspaceName}/${projectName}/Cartridge_Management/Load_Cartridge", parameters: [[$class: 'StringParameterValue', name: 'CARTRIDGE_CLONE_URL', value: "${cartridgeURL}"], [$class: 'StringParameterValue', name: 'SCM_PROVIDER', value: "${scmProvider}"]]
+  build job: "${workspaceName}/${projectName}/Cartridge_Management/Load_Cartridge", parameters: [[$class: 'StringParameterValue', name: 'CARTRIDGE_CLONE_URL', value: "${cartridgeURL}"]]
 }''')
 sandbox()
         }
