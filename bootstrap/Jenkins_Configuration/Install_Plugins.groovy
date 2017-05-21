f7e2bbaf3e948398924ce67e749c4440cc7fbade
nikolaos.karapanos <nikolaos.karapanos@accenture.com>
2017-05-21 18:05:28 +0100

    Added the plugin installation scripts

--- bootstrap/Jenkins_Configuration/Install_Plugins.groovy
+++ bootstrap/Jenkins_Configuration/Install_Plugins.groovy
@@ -1,4 +1,6 @@
 // Constants
+def platformToolsGitURL = "https://github.com/Nikos-K/adop-lite-platform-management.git"
+
 def jenkinsConfigurationFolderName= "/Jenkins_Configuration"
 def jenkinsConfigurationFolder = folder(jenkinsConfigurationFolderName) { displayName('Jenkins Configuration') }
 
@@ -8,4 +10,27 @@ def installPlugins = workflowJob(jenkinsConfigurationFolderName + "/Install_Plug
 // Install Jenkins Plugins
 installPlugins.with{
   description("This job is responsible for installing all the required Jenkins plugins for ADOP Lite.")
+  environmentVariables {
+    keepBuildVariables(true)
+    keepSystemVariables(true)
+  }
+  wrappers {
+    preBuildCleanup()
+  }
+  steps {
+    shell('''#!/bin/bash -e
+
+./Jenkkins_Configuration/install_plugins.sh plugins.txt
+
+''')
+  }
+  scm {
+    git {
+      remote {
+        name("origin")
+        url("${platformToolsGitURL}")
+      }
+      branch("*/master")
+    }
+  }
 }
