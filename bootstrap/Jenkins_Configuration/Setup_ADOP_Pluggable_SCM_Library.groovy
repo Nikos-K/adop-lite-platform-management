8a5cccbee5952cc96b67fc17a13f96fb0a4b1d5f
nikolaos.karapanos <nikolaos.karapanos@accenture.com>
2017-05-24 09:22:48 +0100

    Updating the Pluggable SCM library job definition

--- bootstrap/Jenkins_Configuration/Setup_ADOP_Pluggable_SCM_Library.groovy
+++ bootstrap/Jenkins_Configuration/Setup_ADOP_Pluggable_SCM_Library.groovy
@@ -17,17 +17,6 @@ setupAdopPluggableScmLibrary.with{
   wrappers {
     preBuildCleanup()
   }
-  steps {
-    shell('''#!/bin/bash -ex
-
-echo "Extracting Pluggable library to additonal classpath location: ${PLUGGABLE_SCM_PROVIDER_PATH}"
-cp -r src/main/groovy/pluggable/ ${PLUGGABLE_SCM_PROVIDER_PATH}
-echo "******************"
-
-echo "Library contents: "
-ls ${PLUGGABLE_SCM_PROVIDER_PATH}pluggable/scm/
-''')
-  }
   scm {
     git {
       remote {
@@ -36,5 +25,51 @@ ls ${PLUGGABLE_SCM_PROVIDER_PATH}pluggable/scm/
       }
       branch("*/master")
     }
+  }
+  steps {
+    //systemGroovyCommand(readFileFromWorkspace('add_groovy_profile.template'))
+    shell('''#!/bin/bash -ex
+    mkdir -p ${WORKSPACE}/datastore/pluggable/scm ${WORKSPACE}/job_dsl_additional_classpath/
+    mkdir -p ${WORKSPACE}/datastore/pluggable/scm/CartridgeLoader ${WORKSPACE}/datastore/pluggable/scm/ScmProviders
+
+    echo "Extracting Pluggable library to additonal classpath location: ${WORKSPACE}/job_dsl_additional_classpath/"
+    cp -r ${WORKSPACE}/src/main/groovy/pluggable/ ${WORKSPACE}/job_dsl_additional_classpath/
+    echo "******************"
+
+    echo "Library contents: "
+    ls ${WORKSPACE}/job_dsl_additional_classpath/pluggable/scm/
+
+    echo "Adding Bitbucket SCM Provider property files"
+    tee ${WORKSPACE}/datastore/pluggable/scm/CartridgeLoader/adop-bitbucket.props << EOF
+    loader.id=adop-bitbucket
+    loader.credentialId=pluggable-scm
+    bitbucket.endpoint=innersource.accenture.com
+    bitbucket.endpoint.context=/
+    bitbucket.protocol=https
+    bitbucket.port=443
+    EOF
+
+    tee ${WORKSPACE}/datastore/pluggable/scm/ScmProviders/adop-bitbucket.ssh.props << EOF
+    scm.loader.id=adop-bitbucket
+    scm.id=adop-bitbucket-ssh
+    scm.type=bitbucket
+    scm.protocol=ssh
+    scm.port=22
+    scm.url=https://innersource.accenture.com
+    EOF
+
+    tee ${WORKSPACE}/datastore/pluggable/scm/ScmProviders/adop-bitbucket.https.props << EOF
+    scm.loader.id=adop-bitbucket
+    scm.id=adop-bitbucket-https
+    scm.type=bitbucket
+    scm.protocol=https
+    scm.port=443
+    scm.url=https://innersource.accenture.com
+    EOF''')
+        shell('''#!/bin/bash
+    echo ${PLUGGABLE_SCM_PROVIDER_PATH}
+    wget https://raw.githubusercontent.com/Accenture/adop-jenkins/master/resources/scriptler/retrieve_scm_props.groovy
+    sed -i "s,###SCM_PROVIDER_PROPERTIES_PATH###,$PLUGGABLE_SCM_PROVIDER_PROPERTIES_PATH,g" retrieve_scm_props.groovy
+''')
   }
 }
---
+++ bootstrap/Jenkins_Configuration/add_env_vars.groovy
@@ -0,0 +1,32 @@
+import jenkins.*
+import jenkins.model.*
+import hudson.*
+import hudson.model.*
+
+instance = Jenkins.getInstance()
+globalNodeProperties = instance.getGlobalNodeProperties()
+envVarsNodePropertyList = globalNodeProperties.getAll(hudson.slaves.EnvironmentVariablesNodeProperty.class)
+
+newEnvVarsNodeProperty = null
+envVars = null
+
+if ( envVarsNodePropertyList == null
+        || envVarsNodePropertyList.size() == 0 ) {
+
+    newEnvVarsNodeProperty = new hudson.slaves.EnvironmentVariablesNodeProperty();
+    globalNodeProperties.add(newEnvVarsNodeProperty)
+    envVars = newEnvVarsNodeProperty.getEnvVars()
+} else {
+    envVars = envVarsNodePropertyList.get(0).getEnvVars()
+}
+
+envVars.put("ADOP_PLATFORM_MANAGEMENT_GIT_URL","ssh://git@innersource.accenture.com/adop-e/adop-platform-management.git")
+envVars.put("ADOP_PLATFORM_MANAGEMENT_VERSION", "*/master")
+envVars.put("ADOP_ACL_ENABLED", "false")
+envVars.put("ADOP_LDAP_ENABLED", "false")
+envVars.put("PLUGGABLE_SCM_PROVIDER_PROPERTIES_PATH", "/var/jenkins_home/userContent/datastore/pluggable/scm")
+envVars.put("PLUGGABLE_SCM_PROVIDER_PATH", "/var/jenkins_home/userContent/job_dsl_additional_classpath/")
+envVars.put("CARTRIDGE_SOURCES", "https://raw.githubusercontent.com/Accenture/adop-cartridges/master/cartridges.yml")
+envVars.put("LDAP_ROOTDN", "")
+
+instance.save()
