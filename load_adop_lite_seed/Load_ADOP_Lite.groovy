7aeb0a62234970d8f5479027c92518c5a324917b
nikolaos.karapanos <nikolaos.karapanos@accenture.com>
2017-06-24 16:39:02 +0100

    removing the generate example workspace job

--- load_adop_lite_seed/Load_ADOP_Lite.groovy
+++ load_adop_lite_seed/Load_ADOP_Lite.groovy
@@ -5,14 +5,12 @@ job('Load_ADOP_Lite'){
 	parameters{
     stringParam("ADOP_PLATFORM_REPO_URL","https://github.com/Nikos-K/adop-lite-platform-management.git","The URL of the git repo for Platform Management.")
     booleanParam("SETUP_PLUGGABLE_SCM_LIBRARY", true, "Set to true to setup the ADOP Pluggable SCM Library.")
-    booleanParam("GENERATE_EXAMPLE_WORKSPACE", false, "Should an example workspace be generated? Set this to true if all the required plugins have been installed.")
 	}
 	scm{
 		git{
 			remote{
 				name("origin")
 				url("${ADOP_PLATFORM_REPO_URL}")
-				credentials("adop-jenkins-master")
 			}
 			branch("*/master")
 		}
@@ -21,7 +19,6 @@ job('Load_ADOP_Lite'){
 		preBuildCleanup()
 		maskPasswords()
 		timestamps()
-		sshAgent("adop-jenkins-master")
 	}
 	authenticationToken('gAsuE35s')
 	steps {
@@ -41,23 +38,6 @@ job('Load_ADOP_Lite'){
       steps {
         downstreamParameterized {
           trigger('Jenkins_Configuration/Setup_Adop_Pluggable_Scm_Library') {
-            block {
-              buildStepFailure('FAILURE')
-              failure('FAILURE')
-              unstable('UNSTABLE')
-						}
-					}
-				}
-      }
-    }
-		conditionalSteps {
-      condition {
-        booleanCondition('${GENERATE_EXAMPLE_WORKSPACE}')
-      }
-      runner('Fail')
-      steps {
-        downstreamParameterized {
-          trigger('Platform_Management/Generate_Example_Workspace') {
             block {
               buildStepFailure('FAILURE')
               failure('FAILURE')
