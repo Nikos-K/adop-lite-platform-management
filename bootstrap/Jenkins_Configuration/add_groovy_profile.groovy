eaf5c821fee12e7132afe0503ade2d2e8404a725
nikolaos.karapanos <nikolaos.karapanos@accenture.com>
2017-06-28 15:50:09 +0100

    Updated the groovy_profile

--- bootstrap/Jenkins_Configuration/add_groovy_profile.groovy
+++ bootstrap/Jenkins_Configuration/add_groovy_profile.groovy
@@ -4,50 +4,46 @@ import hudson.tools.*;
 import hudson.plugins.groovy.*;
 
 def env = System.getenv();
-def groovyVersion = env['GROOVY_VERSION']
+def groovyVersion = '2.4.8'
 def groovyVersionList = groovyVersion.split(',')
 
 def instance = Jenkins.getInstance()
 
-Thread.start {
- sleep 10000
+println "--> Configuring Groovy"
+def groovyDescriptor = instance.getDescriptor("hudson.plugins.groovy.GroovyInstallation")
+def groovyInstallations = groovyDescriptor.getInstallations()
 
- println "--> Configuring Groovy"
- def groovyDescriptor = instance.getDescriptor("hudson.plugins.groovy.GroovyInstallation")
- def groovyInstallations = groovyDescriptor.getInstallations()
+groovyVersionList.eachWithIndex {
+version,
+index ->
+def installer = new GroovyInstaller(version)
+def installSourceProperty = new InstallSourceProperty([installer])
 
- groovyVersionList.eachWithIndex {
-  version,
-  index ->
-  def installer = new GroovyInstaller(version)
-  def installSourceProperty = new InstallSourceProperty([installer])
+def name = "ADOP Groovy_" + version
 
-  def name = "ADOP Groovy_" + version
-
-  if (index == 0) {
-   name = "ADOP Groovy"
-  }
-
-  def installation = new GroovyInstallation(
-   name,
-   "", [installSourceProperty]
-  )
-
-  def groovyIntExists = false
-  groovyInstallations.each {
-   currentInstallation = (GroovyInstallation) it
-   if (installation.getName() == currentInstallation.getName()) {
-    groovyIntExists = true
-    println("Found existing installation: " + installation.getName())
-   }
-  }
-
-  if (!groovyIntExists) {
-   groovyInstallations += installation
-  }
- }
-
- groovyDescriptor.setInstallations((GroovyInstallation[]) groovyInstallations)
-
- instance.save()
+if (index == 0) {
+ name = "ADOP Groovy"
 }
+
+def installation = new GroovyInstallation(
+ name,
+ "", [installSourceProperty]
+)
+
+def groovyIntExists = false
+groovyInstallations.each {
+ currentInstallation = (GroovyInstallation) it
+ if (installation.getName() == currentInstallation.getName()) {
+  groovyIntExists = true
+  println("Found existing installation: " + installation.getName())
+ }
+}
+
+if (!groovyIntExists) {
+ groovyInstallations += installation
+}
+}
+
+groovyDescriptor.setInstallations((GroovyInstallation[]) groovyInstallations)
+
+instance.save()
